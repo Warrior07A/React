3108cfc278a16e09bba84b59f2a90c434c90d743
warrior07A <akshatmittalofficial@gmail.com>
2025-07-28 19:15:50 +0530

    defining component and props

--- React-project/src/App.jsx
+++ React-project/src/App.jsx
@@ -1,43 +1,69 @@
-import {useState,useEffect} from "react";
-
 function App(){
-  //conditional rendering
-  const [countervisible,check]=useState(true);
-  return<div>  
- hi there
+return(
+  <div style={{display:"flex",justifyContent:"space-around"}}>   
+    <div>
+    <NewPostComponenet 
+    name={"Allu Arjun"}
+    subtitle={"Actor"}
+    time={"a few years"}
+    /><br></br>
+    <NewPostComponenet
+  name={"Akshat Mittal"}
+  subtitle={"Web Developer"}
+  time={"active "}
 
-useEffect(function(){
-  setInterval(function(){
-    check(c=>!c)
-  },5000)
-},[])
+  /><br></br>
+    <NewPostComponenet/>
+   
+    </div>
+    <div style={{}}>
+    <ProfileComponent/>
+    </div>
+  </div>
 
-{countervisible && <Counter></Counter>}
-
-</div>
-}
-
-function Counter(){
-  const[count,Setcount]=useState(0);
-  console.log("unmounted");
-
-  //guards our inside data from re-rendering
-useEffect(function(){
-  console.log("me one time");
-  setInterval(() => {
-    Setcount(count=>count+1);  //not simply count+1 due to some dependency array error 
-  }, 1000);
-},[])           //if you wont add depedency it would run every time it re -renders
-
-
-function increasecount(){
-	Setcount(count+1);
-}
-
-return <div>
-<h1> {count}</h1>
-<button onClick={increasecount}> INCREASE COUNT</button>
-</div>
-}
  
+)
+
+}
+
+function NewPostComponenet({name,subtitle,time}){
+  return<div style={{backgroundColor:"grey",borderRadius:10,width:200,height:100,display:"flex",padding:20}}>
+  <div>
+
+    <img  style={{width:40,height:40,borderRadius:10}} src={"https://imgs.search.brave.com/dly800JWKoTuDt4vzBhapDXP4kLzz7dk37n0xaY3HR4/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vdmVjdG9y/cy9yYW5kb20tYWN0/cy1vZi1raW5kbmVz/cy1kYXktZW1ibGVt/LWlzb2xhdGVkLXZl/Y3Rvci1zdG9jay12/ZWN0b3ItaWQxMjgw/MDU0NzUzP2s9MjAm/bT0xMjgwMDU0NzUz/JnM9NjEyeDYxMiZ3/PTAmaD1VSnY4XzQy/UlRzcVMwcGpPRHc1/Y3Rkc0FaaFNLUEdP/ZmtZaFlLUU1CYUFZ/PQ"
+    }>
+    </img></div>
+    <div>
+    {name}<br></br>
+    {subtitle}<br></br>
+    {time}
+    <img style={{height:12,width:12}} src={"https://imgs.search.brave.com/fPv5zdG4HXofc7KYNeZob69q9QoXvA18FpN1xm-00-U/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuZnJlZWltYWdl/cy5jb20vaW1hZ2Vz/L2xhcmdlLXByZXZp/ZXdzLzJiOS9lbGVn/YW50LXdhbGwtY2xv/Y2stMDQxMC01NzA1/MTQ1LmpwZz9mbXQ"}></img>
+
+    
+    </div>
+  
+   </div>
+}
+const style1={width:200,height:500}
+function ProfileComponent(){
+
+  return(<div style={{dispaly:"flex",margin:50}}>
+  <div style={style1}>
+    <b>Linkedin News</b>
+    <div>
+    Top Stories
+    </div>
+    <div>
+    jbcjscb
+    </div>
+    csdcscsc
+    sccce
+    cassdasada
+    adsadasd
+    assas
+
+  </div>
+  </div>)
+}
 export default App
+
